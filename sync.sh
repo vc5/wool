88f65b400f1de6d9630e7304aad7b5228d221d1f
Vincent <intelligentvincent@gmail.com>
2022-05-05 03:03:06 +0000

    update2022-5-5

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ## export https_proxy=http://192.168.8.253:1081;export http_proxy=http://192.168.8.253:1081;export all_proxy=socks5://192.168.8.253:1080
-## ql repo https://github.com/vc5/wool.git "dd_|ks_|mt_"  "sync|cookie" "dd_cookie|mtcookie"  main
+## ql repo https://github.com/vc5/wool.git "dd_|ks_|mt_"  "sync|cookie" "dd_cookie|mtCookie"  main
 
 
 ## passerby-b
@@ -20,7 +20,7 @@ sed -i '/task_local/i\const $ = new Env("滴滴果园");\n' ./dd_fruit.js
 #美团买菜果园
 git clone --depth 1 https://github.com/passerby-b/mt_fruit.git  upstream/passerby-b/mt_fruit
 cp upstream/passerby-b/mt_fruit/mt_fruit.js ./mt_fruit.js
-cp upstream/passerby-b/mt_fruit/mtCookie.js ./mtcookie.js
+cp upstream/passerby-b/mt_fruit/mtCookie.js ./mtCookie.js
 sed -i '/task_local/i\const $ = new Env("美团买菜果园");\n' ./mt_fruit.js
 
 ## 清理
